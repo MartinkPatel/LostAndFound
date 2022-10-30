6aea5d0919547d4ef6ec72b8141f85f516a13aeb
Martinkpatel <martin_2101cs43@iitp.ac.in>
2022-10-30 19:29:47 +0530

    Initial commit

---
+++ app/src/main/java/com/martin_kirtan/lostandfound/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.martin_kirtan.lostandfound
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
